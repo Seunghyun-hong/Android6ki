cf4d9d35bb535282e622e6584393789d3887d37d
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2017-07-19 10:57:48 +0900

    커피 주소땡겨오고,, 그리고 잘 작동하는지 확인도 해봤쪄

--- app/src/main/java/com/example/thunder/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/thunder/myapplication/CoffeeActivity.java
@@ -1,7 +1,10 @@
 package com.example.thunder.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CoffeeActivity extends AppCompatActivity {
     //메인
@@ -11,5 +14,18 @@ public class CoffeeActivity extends AppCompatActivity {
 
         //레이아웃 설정
         setContentView(R.layout.activity_coffee);
+
+        // XML 에 있는 View 의 레퍼런스를 가져오는 방법
+        TextView quantityTextView = (TextView) findViewById(R.id.count_coffee);
+//        Button minusButton = (Button) findViewById(R.id.minus_button);
+//        Button plusButton = (Button) findViewById(R.id.plus_button);
+    }
+
+    public void minusButtonClicked(View view) {
+        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pulsButtenClicked(View view) {
+//        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
     }
 }
