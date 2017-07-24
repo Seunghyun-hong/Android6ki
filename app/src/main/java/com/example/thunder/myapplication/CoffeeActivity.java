7ede01777c36eca4cc35287cdf83fb13be49d818
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2017-07-24 15:50:35 +0900

    커피앱인텐트 추가

--- app/src/main/java/com/example/thunder/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/thunder/myapplication/CoffeeActivity.java
@@ -1,9 +1,11 @@
 package com.example.thunder.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -18,6 +20,7 @@ public class CoffeeActivity extends AppCompatActivity {
     private TextView mPriceTextView;
     private CheckBox mWippedCreamCheckBox;
     private int mQuantity = DEFAULT_QUANTITY;
+    private EditText mNameEditText;
 
     private DecimalFormat mFormat = new DecimalFormat("#,##0");
 
@@ -32,7 +35,7 @@ public class CoffeeActivity extends AppCompatActivity {
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
         mPriceTextView = (TextView) findViewById(R.id.price_text);
         mWippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check);
-
+        mNameEditText = (EditText) findViewById(R.id.name_edit);
 //        mQuantityTextView.setText(String.valueOf(mQuantity));
         display();
     }
@@ -40,7 +43,9 @@ public class CoffeeActivity extends AppCompatActivity {
     private void display() {
         mQuantityTextView.setText("" + mQuantity);
 
-        String message = "휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
+        String message = "주문자 : " + mNameEditText.getText().toString();
+        message += "\n====================";
+        message += "\n휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
         message += "\n갯수 : " + mQuantity;
         message += "\n가격 : " + mFormat.format(mQuantity * COFFEE_PRICE) + "원";
 
@@ -62,5 +67,30 @@ public class CoffeeActivity extends AppCompatActivity {
 
     public void onCheckBoxClicked(View view) {
         display();
+    }
+
+
+    public void sendTextMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("text/plaint");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void orderButtonClicked(View view) {
+        mQuantityTextView.setText("" + mQuantity);
+
+        String message = "주문자 : " + mNameEditText.getText().toString();
+        message += "\n====================";
+        message += "\n휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
+        message += "\n갯수 : " + mQuantity;
+        message += "\n가격 : " + mFormat.format(mQuantity * COFFEE_PRICE) + "원";
+
+        mPriceTextView.setText(message);
+
+        sendTextMessage(message);
+
     }
 }
